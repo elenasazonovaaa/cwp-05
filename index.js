19e0357b3d19b31903bed23d133c24c9f13096af
elenasazonovaaa <elenasazonovaaa@gmail.com>
2017-10-03 12:36:11 +0300

    task 02 is created

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const hostname = '127.0.0.1';
 const port = 3005;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Wold!');
+    parseBodyJson(req,(err,payload) =>{
+        const c = {c: payload.a + payload.b};
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(c));
+    });
 });
 
 server.listen(port,hostname,() => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function parseBodyJson(req, cd) {
+    let body = [];
+    req.on('data',function (chunk) {
+       body.push(chunk);
+    }).on('end',function () {
+        body = Buffer.concat(body).toString();
+        let params = JSON.parse(body);
+        cd(null,params);
+    });
+}
